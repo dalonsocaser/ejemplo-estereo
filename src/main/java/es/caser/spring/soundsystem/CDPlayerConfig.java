710f16b36caefeba873b92841dc93bd78fad109b
DALOSA01 <DALOSA01@CASER-5041.caser.local>
2017-08-23 13:06:20 +0200

    Scanning basado en clases, en vez de en paquetes

--- src/main/java/es/caser/spring/soundsystem/CDPlayerConfig.java
+++ src/main/java/es/caser/spring/soundsystem/CDPlayerConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages={"es.caser.spring.soundsystem", "es.caser.spring.video"})
+@ComponentScan(basePackageClasses=CompactDisc.class)
 public class CDPlayerConfig {
 
 }
