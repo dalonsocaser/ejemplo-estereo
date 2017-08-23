fce52653cef619085dd1cd828f45aff8f9523e7f
unknown <DALOSA01@CASER-5041.caser.local>
2017-08-23 13:01:04 +0200

    especificando paquetes manualmente via strings

--- src/main/java/es/caser/spring/soundsystem/CDPlayerConfig.java
+++ src/main/java/es/caser/spring/soundsystem/CDPlayerConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages={"es.caser.spring.soundsystem", "es.caser.spring.video"})
 public class CDPlayerConfig {
 
 }
