613475c32314071355c2302b403299e2ffa208c5
DALOSA01 <DALOSA01@CASER-5041.caser.local>
2017-08-24 11:27:32 +0200

    Importando xml desde java

--- src/main/java/es/caser/spring/cds/CDPlayerConfig.java
+++ src/main/java/es/caser/spring/cds/CDPlayerConfig.java
@@ -1,14 +1,12 @@
 package es.caser.spring.cds;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan
+@ImportResource("classpath:META-INF/context.xml")
 public class CDPlayerConfig {
-	@Bean(name="enriqueIglesiasCd")
-	public CdVirgen enriqueIglesiasCd(){
-		return new CdVirgen("Súbeme la radio", "Enrique Iglesias");
-	}
+	
 }
--- src/main/java/es/caser/spring/cds/CdVirgen.java
+++ src/main/java/es/caser/spring/cds/CdVirgen.java
@@ -1,14 +1,54 @@
 package es.caser.spring.cds;
 
+import java.util.List;
+
 public class CdVirgen implements CompactDisc {
 
 	private String title;
 	private String artist;
-
-	public CdVirgen(String title, String artist) {
+	private List<String> tracks;
+	public CdVirgen(String title, String artist,List<String> tracks) {
 		this.title = title;
 		this.artist = artist;
+		this.tracks=tracks;
 	}
+
+	
+	public CdVirgen() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public List<String> getTracks() {
+		return tracks;
+	}
+
+
+	public void setTracks(List<String> tracks) {
+		this.tracks = tracks;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getArtist() {
+		return artist;
+	}
+
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
 
 	public void play() {
 		System.out.println("Playing " + title + " by " + artist);
