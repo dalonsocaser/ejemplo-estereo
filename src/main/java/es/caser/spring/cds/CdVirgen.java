e58523749a4f2a609e6dc5cc612e2d7e9247be4b
DALOSA01 <DALOSA01@CASER-5041.caser.local>
2017-08-24 10:11:57 +0200

    Agregado listado de pistas xml

--- src/main/java/es/caser/spring/cds/CdVirgen.java
+++ src/main/java/es/caser/spring/cds/CdVirgen.java
@@ -1,17 +1,23 @@
 package es.caser.spring.cds;
 
+import java.util.List;
+
 public class CdVirgen implements CompactDisc {
 
 	private String title;
 	private String artist;
-
-	public CdVirgen(String title, String artist) {
+	private List<String> tracks;
+	public CdVirgen(String title, String artist,List<String> tracks) {
 		this.title = title;
 		this.artist = artist;
+		this.tracks = tracks;
 	}
 
 	public void play() {
 		System.out.println("Playing " + title + " by " + artist);
+		for (String track : tracks) {
+			System.out.println("-Track: " + track);
+		}
 	}
 
 }
