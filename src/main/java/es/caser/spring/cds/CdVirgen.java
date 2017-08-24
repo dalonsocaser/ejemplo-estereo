ba4b6c5a462b09e74f8666c23f3cd8d378ce0217
DALOSA01 <DALOSA01@CASER-5041.caser.local>
2017-08-24 10:20:32 +0200

    xml con properties

--- src/main/java/es/caser/spring/cds/CdVirgen.java
+++ src/main/java/es/caser/spring/cds/CdVirgen.java
@@ -13,6 +13,35 @@ public class CdVirgen implements CompactDisc {
 		this.tracks = tracks;
 	}
 
+	public CdVirgen() {
+		super();		
+	}
+
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public List<String> getTracks() {
+		return tracks;
+	}
+
+	public void setTracks(List<String> tracks) {
+		this.tracks = tracks;
+	}
+
 	public void play() {
 		System.out.println("Playing " + title + " by " + artist);
 		for (String track : tracks) {
